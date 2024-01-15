b4644978d24a151552713b596ebd8d2e6bb8e15c
MarieZedginidze <zedginidze.marie@gmail.com>
2024-01-15 14:36:16 +0400

    added exporting the scene functionality and set preserveDrawingBuffer to true to the renderer initializationin the

--- VanBuilder/src/script.js
+++ VanBuilder/src/script.js
@@ -61,6 +61,7 @@ scene.add(camera);
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  preserveDrawingBuffer: true,
 });
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -370,6 +371,39 @@ function resetScene() {
 let resetBtn = document.getElementById("reset-btn");
 resetBtn.addEventListener("click", resetScene);
 
+function saveAsImage() {
+  var imgData, imgNode;
+
+  try {
+    var strMime = "image/jpeg";
+    var strDownloadMime = "image/octet-stream";
+
+    imgData = renderer.domElement.toDataURL(strMime);
+
+    saveFile(imgData.replace(strMime, strDownloadMime), "test.jpg");
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+}
+var saveFile = function (strData, filename) {
+  var link = document.createElement("a");
+  if (typeof link.download === "string") {
+    document.body.appendChild(link); //Firefox requires the link to be in the body
+    link.download = filename;
+    link.href = strData;
+    link.click();
+    document.body.removeChild(link); //remove the link when done
+  } else {
+    location.replace(uri);
+  }
+};
+
+/**
+ * Export the Scene
+ *  */
+let exportBtn = document.getElementById("export-btn");
+exportBtn.addEventListener("click", saveAsImage);
 /**
  * Animate
  */
